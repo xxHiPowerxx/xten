65046c375531a5abadce69ef4b7edcf1bbf61d7d
Ricky Adams <ra@main.local>
2020-03-09 14:18:56 -0700

    added advanced-custom-fields dependency in theme

--- dev/functions.php
+++ dev/functions.php
@@ -349,6 +349,29 @@ require get_template_directory() . '/inc/site-settings.php';
 require get_template_directory() . '/inc/editor-role.php';
 
 /**
+ * Include ACF Dependancy
+ */
+// Define path and URL to the ACF plugin.
+define( 'MY_ACF_PATH', get_template_directory() . '/vendor/advanced-custom-fields/' );
+define( 'MY_ACF_URL', get_theme_file_uri() . '/vendor/advanced-custom-fields/' );
+
+// Include the ACF plugin.
+include_once( MY_ACF_PATH . 'acf.php' );
+
+// Customize the url setting to fix incorrect asset URLs.
+add_filter('acf/settings/url', 'my_acf_settings_url');
+function my_acf_settings_url( $url ) {
+    return MY_ACF_URL;
+}
+
+// (Optional) Hide the ACF admin menu item.
+add_filter('acf/settings/show_admin', 'my_acf_settings_show_admin');
+function my_acf_settings_show_admin( $show_admin ) {
+    return false;
+}
+
+
+/**
  * Load ACF Fields
  */
 add_filter( 'acf/settings/load_json', 'xten_json_load_point' );
