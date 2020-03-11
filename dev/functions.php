3d783eeb2d3187def86852c4f4c79ed1bfc28516
Ricky Adams <ra@main.local>
2020-03-11 09:22:47 -0700

    commented out function that hides acf custom fields options, and fixed filter that fixes acf url settings.

--- dev/functions.php
+++ dev/functions.php
@@ -361,16 +361,16 @@ define( 'MY_ACF_URL', get_theme_file_uri() . '/vendor/advanced-custom-fields/' )
 include_once( MY_ACF_PATH . 'acf.php' );
 
 // Customize the url setting to fix incorrect asset URLs.
-add_filter('acf/settings/url', 'my_acf_settings_url');
+add_filter('/acf/settings/url', 'my_acf_settings_url');
 function my_acf_settings_url( $url ) {
     return MY_ACF_URL;
 }
 
 // (Optional) Hide the ACF admin menu item.
-add_filter('acf/settings/show_admin', 'my_acf_settings_show_admin');
-function my_acf_settings_show_admin( $show_admin ) {
-    return false;
-}
+// add_filter('acf/settings/show_admin', 'my_acf_settings_show_admin');
+// function my_acf_settings_show_admin( $show_admin ) {
+//     return false;
+// }
 
 
 /**
