7aad1877dbe27c877519b10501fcf9f673ae0edb
Ricky Adams <ra@main.local>
2020-03-27 14:55:08 -0700

    placed .article-container in home.php rather than inside content

--- dev/home.php
+++ dev/home.php
@@ -44,19 +44,23 @@ get_header(); ?>
 
 							/* Start the Loop */
 							while ( have_posts() ) :
-								the_post();
+								?>
+								<div class="article-container col-md-6">
+									<?php
+									the_post();
 
-								/*
-								* Include the Post-Type-specific template for the content.
-								* If you want to override this in a child theme, then include a file
-								* called content-___.php (where ___ is the Post Type name) and that will be used instead.
-								*/
-								get_template_part( 'template-parts/content', 'archive-post' );
-
+									/*
+									* Include the Post-Type-specific template for the content.
+									* If you want to override this in a child theme, then include a file
+									* called content-___.php (where ___ is the Post Type name) and that will be used instead.
+									*/
+									get_template_part( 'template-parts/content', 'archive-post' );
+									?>
+								</div><!-- .article-container -->
+								<?php
 							endwhile;
-
 							?>
-						</div>
+						</div><!-- /.archive-container -->
 						<?php
 
 						the_posts_navigation(
--- dev/template-parts/content-archive-post.php
+++ dev/template-parts/content-archive-post.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Template part for displaying archive posts
+ * Template part for displaying a single archive-post in a collection of archive-posts
  *
  * @link https://codex.wordpress.org/Template_Hierarchy
  *
@@ -9,95 +9,91 @@
 
 ?>
 
-<div class="article-container col-md-6">
-
-	<article id="post-<?php the_ID(); ?>" <?php post_class( 'card-style content-area d-flex flex-column' ); ?>>
-		<div class="featured-image">
-			<a href="<?php echo esc_url( get_permalink() ); ?>" rel="bookmark">
-				<?php
-				if ( has_post_thumbnail() ) :
-					the_post_thumbnail( 'archive-thumbnail' );
-				elseif ( get_theme_mod( 'default_post_image' ) ) :
-					echo wp_get_attachment_image( get_theme_mod( 'default_post_image' ), 'archive-thumbnail' );
-				else :
-					?>
-					<?php
-					$thumb_id = get_post_thumbnail_id( get_the_ID() );
-					$alt      = esc_attr( get_post_meta( $thumb_id, '_wp_attachment_image_alt', true ) );
-					?>
-					<img src="<?php echo get_template_directory_uri(); ?>/images/426x240.png" class="attachment-archive-thumbnail" alt="<?php echo $alt; ?>" width="426" height="240" />
-					<?php
-				endif;
+<article id="post-<?php the_ID(); ?>" <?php post_class( 'card-style content-area d-flex flex-column' ); ?>>
+	<div class="featured-image">
+		<a href="<?php echo esc_url( get_permalink() ); ?>" rel="bookmark">
+			<?php
+			if ( has_post_thumbnail() ) :
+				the_post_thumbnail( 'archive-thumbnail' );
+			elseif ( get_theme_mod( 'default_post_image' ) ) :
+				echo wp_get_attachment_image( get_theme_mod( 'default_post_image' ), 'archive-thumbnail' );
+			else :
 				?>
-			</a>
-		</div>
-		<div class="post-body d-flex flex-column">
-
-			<header class="entry-header">
 				<?php
+				$thumb_id = get_post_thumbnail_id( get_the_ID() );
+				$alt      = esc_attr( get_post_meta( $thumb_id, '_wp_attachment_image_alt', true ) );
+				?>
+				<img src="<?php echo get_template_directory_uri(); ?>/images/426x240.png" class="attachment-archive-thumbnail" alt="<?php echo $alt; ?>" width="426" height="240" />
+				<?php
+			endif;
+			?>
+		</a>
+	</div>
+	<div class="post-body d-flex flex-column">
 
-				if ( 'post' === get_post_type() ) :
+		<header class="entry-header">
+			<?php
+
+			if ( 'post' === get_post_type() ) :
+				?>
+				<div class="entry-meta xten-highlight-font">
+					<?php
+					if ( is_singular() ) :
+						the_title( '<h1 class="entry-title">', '</h1>' );
+					else :
+						the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
+					endif;
 					?>
-					<div class="entry-meta xten-highlight-font">
+					<div class="post-date">
 						<?php
-						if ( is_singular() ) :
-							the_title( '<h1 class="entry-title">', '</h1>' );
-						else :
-							the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
-						endif;
+							xten_posted_on();
 						?>
-						<div class="post-date">
-							<?php
-								xten_posted_on();
-							?>
-						</div>
-					</div><!-- .entry-meta -->
-					<?php
-				endif;
-				?>
-			</header><!-- .entry-header -->
-
-			<div class="entry-content">
-				<?php
-
-				the_excerpt(
-					sprintf(
-						wp_kses(
-							/* translators: %s: Name of current post. Only visible to screen readers */
-							__( 'Continue reading<span class="screen-reader-text"> "%s"</span>', 'xten' ),
-							array(
-								'span' => array(
-									'class' => array(),
-								),
-							)
-						),
-						get_the_title()
-					)
-				);
-
-				wp_link_pages(
-					array(
-						'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'xten' ),
-						'after'  => '</div>',
-					)
-				);
-				?>
-			</div><!-- .entry-content -->
-
-			<footer class="entry-footer xten-highlight-font">
-				<div class="cat-link-container d-flex flex-row justify-content-between">
-					<div class="post-category">
-						<?php xten_post_categories(); ?>
 					</div>
-					<a href="<?php echo esc_url( get_the_permalink( $post->ID ) ); ?>" title="<?php echo get_the_title(); ?>" class="post-link xten-theme-color-bg material-btn"><i class="fas fa-arrow-right"></i><span class="hide-me"><?php echo get_the_title(); ?></span></a>
-				</div>
+				</div><!-- .entry-meta -->
 				<?php
-				xten_edit_post_link();
-				?>
-			</footer><!-- .entry-footer -->
+			endif;
+			?>
+		</header><!-- .entry-header -->
 
-		</div>
+		<div class="entry-content">
+			<?php
 
-	</article><!-- #post-<?php the_ID(); ?> -->
+			the_excerpt(
+				sprintf(
+					wp_kses(
+						/* translators: %s: Name of current post. Only visible to screen readers */
+						__( 'Continue reading<span class="screen-reader-text"> "%s"</span>', 'xten' ),
+						array(
+							'span' => array(
+								'class' => array(),
+							),
+						)
+					),
+					get_the_title()
+				)
+			);
 
-</div><!-- .article-container -->
+			wp_link_pages(
+				array(
+					'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'xten' ),
+					'after'  => '</div>',
+				)
+			);
+			?>
+		</div><!-- .entry-content -->
+
+		<footer class="entry-footer xten-highlight-font">
+			<div class="cat-link-container d-flex flex-row justify-content-between">
+				<div class="post-category">
+					<?php xten_post_categories(); ?>
+				</div>
+				<a href="<?php echo esc_url( get_the_permalink( $post->ID ) ); ?>" title="<?php echo get_the_title(); ?>" class="post-link xten-theme-color-bg material-btn"><i class="fas fa-arrow-right"></i><span class="hide-me"><?php echo get_the_title(); ?></span></a>
+			</div>
+			<?php
+			xten_edit_post_link();
+			?>
+		</footer><!-- .entry-footer -->
+
+	</div>
+
+</article><!-- #post-<?php the_ID(); ?> -->
