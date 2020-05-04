d57bd49bb518e18eef0c21dcd8f01f7fdfc89b4a
Ricky Adams <ra@main.local>
2020-05-04 16:55:18 -0700

    removed loadsplash functions for now, made .collapses accessible

--- dev/js/main.js
+++ dev/js/main.js
@@ -5,48 +5,48 @@
  *
  * @since 1.0.0
  */
-( function hideSplash() {
-	setTimeout( function() {
-		var loadSplash = document.getElementById( 'load-splash' ),
-			keyDownHandler = function() {
-				loadSplash.classList.add( 'hiding' );
-				this.removeEventListener( 'keydown', keyDownHandler, false );
+(function hideSplash() {
+	setTimeout(function () {
+		var loadSplash = document.getElementById('load-splash'),
+			keyDownHandler = function () {
+				loadSplash.classList.add('hiding');
+				this.removeEventListener('keydown', keyDownHandler, false);
 			};
-		if ( loadSplash ) {
-			loadSplash.addEventListener( 'click', function() {
-				this.classList.add( 'hiding' );
+		if (loadSplash) {
+			loadSplash.addEventListener('click', function () {
+				this.classList.add('hiding');
 			});
-			window.addEventListener( 'keydown', keyDownHandler, false );
+			window.addEventListener('keydown', keyDownHandler, false);
 		}
 	});
-}() );
-( function( $ ) {
-	$( document ).ready( function() {
+}());
+(function ($) {
+	$(document).ready(function () {
 
 		// Local Variables.
 		var windowLoaded = false,
 			transitionEndEvent = 'webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend',
-			$loadSplash = $( '#load-splash' ),
+			$loadSplash = $('#load-splash'),
 			loadSplash = $loadSplash[0];
 
 		function detectMac() {
-			if ( -1 != navigator.userAgent.indexOf( 'Mac OS X' ) ) {
-				$( 'body' ).addClass( 'mac' );
+			if (-1 != navigator.userAgent.indexOf('Mac OS X')) {
+				$('body').addClass('mac');
 				return true;
 			}
 		}
 
 		function detectIe() {
 			if (
-				/MSIE 10/i.test( navigator.userAgent ) ||
-				/MSIE 9/i.test( navigator.userAgent ) ||
-				/rv:11.0/i.test( navigator.userAgent ) ||
-				/Edge\/\d./i.test( navigator.userAgent )
+				/MSIE 10/i.test(navigator.userAgent) ||
+				/MSIE 9/i.test(navigator.userAgent) ||
+				/rv:11.0/i.test(navigator.userAgent) ||
+				/Edge\/\d./i.test(navigator.userAgent)
 			) {
-				if ( /Edge\/\d./i.test( navigator.userAgent ) ) {
-					$( 'body' ).addClass( 'edge' );
+				if (/Edge\/\d./i.test(navigator.userAgent)) {
+					$('body').addClass('edge');
 				} else {
-					$( 'body' ).addClass( 'ie' );
+					$('body').addClass('ie');
 				}
 				return true;
 			}
@@ -54,8 +54,8 @@
 
 		function detectIOS() {
 			var userAgent = navigator.userAgent || navigator.vendor || window.opera;
-			if ( /iPad|iPhone|iPod/.test( userAgent ) && ! window.MSStream ) {
-				$( 'body' ).addClass( 'ios' );
+			if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
+				$('body').addClass('ios');
 				return 'iOS';
 			}
 		}
@@ -73,17 +73,17 @@
 			var minHeight;
 			var headerHeight;
 			var colophon;
-			if ( 0 < $( '.sizeContent' ).length ) {
-				headerHeight = $( '#masthead' ).outerHeight();
-				colophon = $( '#colophon' );
+			if (0 < $('.sizeContent').length) {
+				headerHeight = $('#masthead').outerHeight();
+				colophon = $('#colophon');
 				minHeight =
-					$( window ).outerHeight() -
+					$(window).outerHeight() -
 					headerHeight -
-					colophon.outerHeight( true );
-				if ( 0 < $( '#wpadminbar:visible' ).length ) {
-					minHeight += -$( '#wpadminbar' ).outerHeight();
+					colophon.outerHeight(true);
+				if (0 < $('#wpadminbar:visible').length) {
+					minHeight += -$('#wpadminbar').outerHeight();
 				}
-				$( '.sizeContent' ).css( 'min-height', minHeight );
+				$('.sizeContent').css('min-height', minHeight);
 			}
 		}
 
@@ -91,51 +91,58 @@
 		 * Show alerts that have not been closed and stored in cookies.
 		 */
 		function alerts() {
-			$( '.xten-alert' ).each( function( index ) {
-				var alertID = $( this ).data( 'alert-id' );
-				var alertCookie = Cookies.get( 'alert-' + alertID );
+			$('.xten-alert').each(function (index) {
+				var alertID = $(this).data('alert-id');
+				var alertCookie = Cookies.get('alert-' + alertID);
 
-				if ( ! alertCookie ) {
-					$( this ).show();
+				if (!alertCookie) {
+					$(this).show();
 				}
 			});
 
-			$( '.xten-alert .close' ).click( function( e ) {
-				var alertID = $( this )
-					.closest( '.xten-alert' )
-					.data( 'alert-id' );
-				var session = $( this )
-					.closest( '.xten-alert' )
-					.data( 'alert-persist-sessions' );
+			$('.xten-alert .close').click(function (e) {
+				var alertID = $(this)
+					.closest('.xten-alert')
+					.data('alert-id');
+				var session = $(this)
+					.closest('.xten-alert')
+					.data('alert-persist-sessions');
 
-				if ( session ) {
-					Cookies.set( 'alert-' + alertID, 'closed' );
+				if (session) {
+					Cookies.set('alert-' + alertID, 'closed');
 				} else {
-					Cookies.set( 'alert-' + alertID, 'closed', { expires: 365 });
+					Cookies.set('alert-' + alertID, 'closed', { expires: 365 });
 				}
 			});
 		}
 
 		function hideLoadSplash() {
-			function addHiding( elem ) {
-				elem.classList.add( 'hiding' );
+			function addHiding(elem) {
+				elem.classList.add('hiding');
 			}
-			$( loadSplash ).on( transitionEndEvent, function() {
-				this.classList.remove( 'loading' );
+			$(loadSplash).on(transitionEndEvent, function () {
+				this.classList.remove('loading');
 			});
-			window.addEventListener( 'finishWork', function() {
+			window.addEventListener('finishWork', function () {
 				var workStarted = window.workStarted || {},
-					workFinished = 0 === Object.keys( workStarted ).length &&
+					workFinished = 0 === Object.keys(workStarted).length &&
 						workStarted.constructor === Object;
-				if ( loadSplash && workFinished ) {
-					addHiding( loadSplash );
+				if (loadSplash && workFinished) {
+					addHiding(loadSplash);
 				}
 			});
 		}
 		function showLoadSplash() {
-			$loadSplash.addClass( 'unloading' );
-			setTimeout( function() {
-				$loadSplash.removeClass( 'hiding' );
+			$loadSplash.addClass('unloading');
+			setTimeout(function () {
+				$loadSplash.removeClass('hiding');
+			});
+		}
+		function makeCollapseAccessible() {
+			$('[data-toggle="collapse"]').on('keyup', function (e) {
+				if (e.which === 13 || e.which === 32) {
+					$(this).trigger('click');
+				}
 			});
 		}
 
@@ -143,83 +150,84 @@
 			detectPlatform();
 			sizeContent();
 			alerts();
-			hideLoadSplash();
+			// hideLoadSplash();
+			makeCollapseAccessible();
 		}
 
 		/**
 		 * Fire Resize functions a second time to adjust for browser repaint.
 		 */
-		function redundantResize( func, time ) {
-			setTimeout( function() {
+		function redundantResize(func, time) {
+			setTimeout(function () {
 				func;
-			}, time );
+			}, time);
 		}
 
 		function resizeFuncs() {
 			sizeContent();
-			redundantResize( sizeContent(), 1000 );
+			redundantResize(sizeContent(), 1000);
 		}
 
 		function loadFuncs() {
-			if ( false === windowLoaded ) {
+			if (false === windowLoaded) {
 				sizeContent();
-				window.dispatchEvent( new CustomEvent( 'finishWork' ) );
+				window.dispatchEvent(new CustomEvent('finishWork'));
 			}
-			return ( windowLoaded = true );
+			return (windowLoaded = true);
 		}
 
 		// Fire Load Functions if 10 seconds have passed from script load. (for slow connections).
-		setTimeout( function() {
+		setTimeout(function () {
 			loadFuncs();
-		}, 10000 );
+		}, 10000);
 
 		readyFuncs();
-		$( window )
-			.resize( function() {
+		$(window)
+			.resize(function () {
 				resizeFuncs();
 			})
-			.on( 'load', function() {
+			.on('load', function () {
 				loadFuncs();
 			})
-			.on( 'beforeunload', function( event ) {
+			/*.on( 'beforeunload', function( event ) {
 				showLoadSplash( event );
-			});
+			})*/;
 	});
 
-	function handleFirstTab( e ) {
-		if ( 9 === e.keyCode ) {
-			document.body.classList.add( 'user-is-tabbing' );
+	function handleFirstTab(e) {
+		if (9 === e.keyCode) {
+			document.body.classList.add('user-is-tabbing');
 
-			window.removeEventListener( 'keydown', handleFirstTab );
-			window.addEventListener( 'mousedown', handleMouseDownOnce );
+			window.removeEventListener('keydown', handleFirstTab);
+			window.addEventListener('mousedown', handleMouseDownOnce);
 		}
 	}
 
 	function handleMouseDownOnce() {
-		document.body.classList.remove( 'user-is-tabbing' );
+		document.body.classList.remove('user-is-tabbing');
 
-		window.removeEventListener( 'mousedown', handleMouseDownOnce );
-		window.addEventListener( 'keydown', handleFirstTab );
+		window.removeEventListener('mousedown', handleMouseDownOnce);
+		window.addEventListener('keydown', handleFirstTab);
 	}
 
-	window.addEventListener( 'keydown', handleFirstTab );
+	window.addEventListener('keydown', handleFirstTab);
 
 	// Polyfill for custom events.
-	( function() {
+	(function () {
 
-		if ( 'function' === typeof window.CustomEvent ) {
+		if ('function' === typeof window.CustomEvent) {
 			return false;
 		}
 
-		function CustomEvent( event, params ) {
-			var evt = document.createEvent( 'CustomEvent' );
+		function CustomEvent(event, params) {
+			var evt = document.createEvent('CustomEvent');
 			params = params || { bubbles: false, cancelable: false, detail: undefined };
-			evt.initCustomEvent( event, params.bubbles, params.cancelable, params.detail );
+			evt.initCustomEvent(event, params.bubbles, params.cancelable, params.detail);
 			return evt;
 		}
 
 		CustomEvent.prototype = window.Event.prototype;
 
 		window.CustomEvent = CustomEvent;
-	}() );
-}( jQuery ) );
+	}());
+}(jQuery));
