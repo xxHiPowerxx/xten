cf48069e218aeb43672f457a86666a86e17a8bd9
Ricky Adams <ra@main.local>
2020-12-18 14:36:55 -0800

    added validation for xten_primary_json_save_point()

--- dev/save-acf-fields.php
+++ dev/save-acf-fields.php
@@ -1,12 +1,14 @@
 <?php
-// Save fields. REMOVE THIS FOR PRODUCTION!
-add_filter( 'acf/settings/save_json', 'xten_primary_json_save_point' );
 
-function xten_primary_json_save_point( $path ) {
+if ( ! function_exists( 'xten_primary_json_save_point' ) ) :
+	function xten_primary_json_save_point( $path ) {
 
-    // update path
-    $path = get_template_directory() . '/acf-json';
+		// update path
+		$path = get_template_directory() . '/acf-json';
 
-    // return
-    return $path;
-}
+		// return
+		return $path;
+	}
+	// Save fields. REMOVE THIS FOR PRODUCTION!
+	add_filter( 'acf/settings/save_json', 'xten_primary_json_save_point' );
+endif; // endif ( ! function_exists( 'xten_primary_json_save_point' ) ) :
