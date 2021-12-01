e3a47d6c31f0e2bbd5b68cab9296bf810ab41259
xxHiPowerxx <rickyadams432@gmail.com>
2021-12-01 15:33:30 -0800

    added xten_get_post_meta_description()

--- dev/inc/utility-functions.php
+++ dev/inc/utility-functions.php
@@ -629,6 +629,44 @@ class XTenUtilities {
 			add_filter( 'the_content', 'check_the_content_for_fancybox', 1 );
 		endif; // endif ( ! function_exists( 'check_the_content_for_fancybox' ) ) :
 
+		if ( ! function_exists( 'xten_get_post_meta_description' ) ) :
+			/**
+			 * Get Meta Description, Excerpt, or create an excerpt.
+			 * @param obj - $post Post Object
+			 * @return string - post Meta Description, Excerpt, or create an excerpt.
+			 */
+			function xten_get_post_meta_description( $post ) {
+				if ( ! $post ) :
+					global $post;
+				endif;
+
+				$post_id = $post->ID;
+				$description = get_post_meta($post_id, '_yoast_wpseo_metadesc', true);
+
+				if ( empty( $description ) ) :
+					setup_postdata( $post );
+					$meta_id = 'metadescription_17587';
+					$xten_seo_description = get_post_meta( $post_id, $meta_id, true );
+					$description = ! empty( $xten_seo_description ) ?
+						$xten_seo_description :
+						null;
+				endif;
+
+				if ( empty( $description ) ) :
+					$description = $post->post_excerpt;
+				endif;
+
+				if ( empty( $description ) ) :
+					$description = xten_trim_string(
+						$post->post_content,
+						30
+					);
+				endif;
+
+				return $description;
+			}
+		endif; // endif ( ! function_exists( 'xten_get_post_meta_description' ) ) :
+
 	}
 	
 }
