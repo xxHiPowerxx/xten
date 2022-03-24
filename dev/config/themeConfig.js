6d64111d846a424e3ff2e5a075871af00c4c7185
xxHiPowerxx <rickyadams432@gmail.com>
2022-03-24 14:13:25 -0700

    formatted for ESLint

--- dev/config/themeConfig.js
+++ dev/config/themeConfig.js
@@ -1,23 +1,24 @@
-"use strict";
+'use strict';
 
 module.exports = {
 	theme: {
-		slug: "xten",
-		name: "XTen",
-		author: "Ricky Adams"
+		slug: 'xten',
+		name: 'XTen',
+		author: 'Ricky Adams'
 	},
 	dev: {
 		browserSync: {
 			live: true,
-			proxyURL: "localhost/illegalbankfees/",
-			bypassPort: "9999"
+			proxyURL: 'localhost/illegalbankfees/',
+			bypassPort: '9999'
 		},
 		browserslist: [
+
 			// See https://github.com/browserslist/browserslist
-			"> 1%",
-			"last 3 versions",
-			"Firefox >= 20",
-			"iOS >=7"
+			'> 1%',
+			'last 3 versions',
+			'Firefox >= 20',
+			'iOS >=7'
 		],
 		debug: {
 			styles: false, // Render verbose CSS for debugging.
